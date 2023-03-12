7387c91f39bc03e330092eec2c8637db070c5828
codeyzx <yahyatruth@gmail.com>
2023-03-12 11:35:04 +0700

    feat: add auction history struct

--- model/request/auction_history-request.go
+++ model/request/auction_history-request.go
@@ -1,5 +1,11 @@
 package request
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type AuctionHistoryCreateRequest struct {
 	AuctionID uint `json:"auctionId"`
 	UserId    uint `json:"userId"`
@@ -8,4 +14,16 @@ type AuctionHistoryCreateRequest struct {
 
 type AuctionHistoryUpdateRequest struct {
 	Price int `json:"price"`
+}
+
+type AuctionHistory struct {
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	AuctionID uint           `json:"-"`
+	Auction   Auction        `json:"auction"`
+	UserId    uint           `json:"-"`
+	User      User           `json:"user"`
+	Price     int            `json:"price"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index,column:deleted_at"`
 }
