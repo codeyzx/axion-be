48714b18367801e1fd9045d0e47294256a47071a
codeyzx <yahyatruth@gmail.com>
2023-03-12 11:35:47 +0700

    feat: add bidder field

--- model/request/auction-request.go
+++ model/request/auction-request.go
@@ -10,18 +10,18 @@ type AuctionCreateRequest struct {
 	Status       string `json:"status"`
 	BiddersCount int    `json:"biddersCount"`
 	UserId       uint   `json:"userId" form:"userId" validate:"required"`
+	BidderId     uint   `json:"bidderId" form:"bidderId"`
 	EndAt        string `json:"endAt"`
 	ProductName  string `json:"productName" validate:"required"`
 	Description  string `json:"description"`
 	Price        int    `json:"price" validate:"required"`
 	Image        string `json:"image"`
-	// Product      Product `json:"product" validate:"required"`
 }
 
 type AuctionUpdateRequest struct {
-	Name      string        `json:"name"`
-	LastPrice int           `json:"lastPrice"`
-	UserId    uint          `json:"userId"`
-	Status    entity.Status `json:"status"`
-	EndAt     string        `json:"endAt"`
+	Name         string        `json:"name"`
+	LastPrice    int           `json:"lastPrice"`
+	Status       entity.Status `json:"status"`
+	BiddersCount int           `json:"biddersCount"`
+	EndAt        string        `json:"endAt"`
 }
